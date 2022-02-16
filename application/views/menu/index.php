23c857a793455edb84c415545258974f2f9400e5
kadi.hore <kadi.hore@gmail.com>
2022-02-16 13:36:04 +0700

    first commit

---
+++ application/views/menu/index.php
@@ -0,0 +1,72 @@
+<!-- Begin Page Content -->
+<div class="container-fluid">
+
+    <!-- Page Heading -->
+    <h1 class="h3 mb-4 text-gray-800"><?= $title; ?></h1>
+
+    <div class="row">
+        <div class="col-lg-6">
+
+            <?= form_error('menu', '<div class="alert alert-danger" role="alert">', '</div>'); ?>
+            <?= $this->session->flashdata('menu'); ?>
+
+            <a href="" class="btn btn-primary mb-3" data-toggle="modal" data-target="#modalNewMenu">Add new menu</a>
+
+            <table class="table table-hover">
+                <thead>
+                    <tr>
+                        <th scope="col">#</th>
+                        <th scope="col">Menu</th>
+                        <th scope="col">Action</th>
+                        <th></th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <?php $i = 1; ?>
+                    <?php foreach ($menu as $m) : ?>
+                        <tr>
+                            <th scope="row"><?= $i;  ?></th>
+                            <td><?= $m['menu']; ?></td>
+                            <td>
+                                <a href="" class="badge badge-success">edit</a>
+                                <a href="" class="badge badge-danger">delete</a>
+                            </td>
+                        </tr>
+                        <?php $i++; ?>
+                    <?php endforeach; ?>
+                </tbody>
+            </table>
+
+
+        </div>
+    </div>
+
+</div>
+<!-- /.container-fluid -->
+
+</div>
+<!-- End of Main Content -->
+
+<!-- Modal add menu -->
+<div class="modal fade" id="modalNewMenu" tabindex="-1" aria-labelledby="modalNewMenuLabel" aria-hidden="true">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="modalNewMenuLabel">Add New Menu</h5>
+                <button type="button" class="btn-close" data-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <form action="<?= base_url('menu'); ?>" method="POST">
+                <div class="modal-body">
+                    <div class="form-group">
+                        <label for="formGroupExampleInput" class="form-label">Menu name</label>
+                        <input type="text" class="form-control" id="menu" name="menu" placeholder="Menu Name">
+                    </div>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                    <button type="submit" class="btn btn-primary">Add</button>
+                </div>
+            </form>
+        </div>
+    </div>
+</div>
