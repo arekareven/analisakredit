eec2adb603ecc0c5a5d5c6e6eb9b5e733b6dc604
kadi.hore <kadi.hore@gmail.com>
2022-05-27 16:54:03 +0700

    dummy-2.2

--- application/controllers/Pdf_capi.php
+++ application/controllers/Pdf_capi.php
@@ -6,13 +6,47 @@ class Pdf_capi extends CI_Controller
     {
         parent::__construct();
         $this->load->model('m_kredit');
-        $this->load->library('Pdf'); // MEMANGGIL LIBRARY YANG KITA BUAT TADI
+        $this->load->library('Pdf'); // MEMANGGIL LIBRARY
     }
 
     function index()
     {
+        //Pemasukan
+        $pemasukan = $this->db->query("SELECT * FROM cashflow_b WHERE MID(kode_perkiraan,1,3) = '4.1'");
+        foreach ($pemasukan->result() as $row) {
+            $array_pemasukan[] = $row->saldo;
+        }
+        $sum_pemasukan = array_sum($array_pemasukan);
+
+        //Pengeluaran
+        $pengeluaran = $this->db->query("SELECT * FROM cashflow_b WHERE MID(kode_perkiraan,1,3) = '5.1'&&'5.2'&&'5.3'");
+        foreach ($pengeluaran->result() as $row) {
+            $array_pengeluaran[] = $row->saldo;
+        }
+        $sum_pengeluaran = array_sum($array_pengeluaran);
+        $surplus_usaha = $sum_pemasukan - $sum_pengeluaran;
+
+        //Pengeluaran lain, problem = jika tidak ada data pengeluaran lain $array_penegeluaran_lain akan error karena dianggap kosong
+        $pengeluaran_lain = $this->db->query("SELECT * FROM cashflow_b WHERE MID(kode_perkiraan,1,3) = '5.4'");
+        foreach ($pengeluaran_lain->result() as $row) {
+            $array_pengeluaran_lain[] = $row->saldo;
+        }
+        $sum_pengeluaran_lain = array_sum($array_pengeluaran_lain);
+
+        //Pengeluaran angsuran, problem = jika tidak ada data pengeluaran angsuran $array_penegeluaran_angsuran akan error karena dianggap kosong
+        $pengeluaran_angsuran = $this->db->query("SELECT * FROM cashflow_b WHERE MID(kode_perkiraan,1,3) = '5.5'");
+        foreach ($pengeluaran_angsuran->result() as $row) {
+            $array_pengeluaran_angsuran[] = $row->saldo;
+        }
+        $sum_pengeluaran_angsuran = array_sum($array_pengeluaran_angsuran);
+
+        //Rugi laba, problem = jika tidak ada data pengeluaran lain / data pengeluaran angsuran akan error karena dianggap kosong
+        $rugi_laba = $sum_pemasukan - ($sum_pengeluaran + $sum_pengeluaran_lain + $sum_pengeluaran_angsuran);
+
+
+        //PDF CAPITAL
         $pdf = new FPDF('P', 'mm', 'Letter');
-        $pdf->SetAutoPageBreak(false);
+        $pdf->SetAutoPageBreak(true);
         // membuat halaman baru
         $pdf->AddPage();
         // margin
@@ -25,6 +59,7 @@ class Pdf_capi extends CI_Controller
                                         JOIN latar_belakang ON capital_b.id_lb=latar_belakang.id_lb
                                         WHERE capital_b.id_lb='$id_lb'");
         foreach ($lb->result() as $data) {
+            $modal = $data->total_aset - ($data->total_hutang + $data->total_at + $rugi_laba);
 
             $pdf->SetFont('Times', 'B', 12);
             $pdf->Cell(79, 5.5, '3. CAPITAL (sebelum memperoleh kredit)', 0, 1, '');
@@ -102,7 +137,7 @@ class Pdf_capi extends CI_Controller
             $pdf->SetFont('Times', 'B', 12);
             $pdf->Cell(66, 5.5, 'Laba / Rugi', 0, 0, '');
             $pdf->Cell(10, 5.5, 'Rp.', 0, 0, '');
-            $pdf->Cell(46, 5.5, number_format($data->laba_rugi), 0, 1);
+            $pdf->Cell(46, 5.5, number_format($rugi_laba), 0, 1);
             $pdf->SetFont('Times', '', 12);
             $pdf->Cell(44, 5.5, 'Lahan Garap', 0, 0, '');
             $pdf->Cell(10, 5.5, 'Rp.', 0, 0, '');
@@ -113,7 +148,7 @@ class Pdf_capi extends CI_Controller
             $pdf->SetFont('Times', 'B', 12);
             $pdf->Cell(66, 5.5, 'Modal Usaha', 0, 0, '');
             $pdf->Cell(10, 5.5, 'Rp.', 0, 0, '');
-            $pdf->Cell(46, 5.5, number_format($data->modal), 0, 1);
+            $pdf->Cell(46, 5.5, number_format($modal), 0, 1);
             $pdf->SetFont('Times', '', 12);
             $pdf->Cell(44, 5.5, 'Kendaraan Operasional', 0, 0, '');
             $pdf->Cell(10, 5.5, 'Rp.', 0, 0, '');
@@ -152,7 +187,7 @@ class Pdf_capi extends CI_Controller
             $pdf->Line(86, 194, 110, 194);
             $pdf->Cell(66, 5.5, 'Harta', 0, 0, '');
             $pdf->Cell(10, 5.5, 'Rp.', 0, 0, '');
-            $pdf->Cell(46, 5.5, number_format($data->harta), 0, 1);
+            $pdf->Cell(46, 5.5, number_format($data->total_at), 0, 1);
             $pdf->Line(110, 173, 160, 173);
             $pdf->Line(160, 173, 186, 186);
             $pdf->Line(186, 186, 210, 186);
@@ -163,55 +198,189 @@ class Pdf_capi extends CI_Controller
             $pdf->Cell(24, 5.5, number_format($data->total_aset), 0, 0);
             $pdf->Cell(66, 5.5, 'TOTAL KEWAJIBAN', 0, 0, '');
             $pdf->Cell(10, 5.5, 'Rp.', 0, 0, '');
-            $pdf->Cell(46, 5.5, number_format($data->total_kjb), 0, 1);
+            $pdf->Cell(46, 5.5, number_format($data->total_aset), 0, 1);
             $pdf->Cell(10, 7, '', 0, 1);
 
 
             $pdf->SetFont('Times', 'B', 12);
             $pdf->Cell(79, 5.5, '4. CASH FLOW (sebelum memperoleh kredit)', 0, 1, '');
             $pdf->Cell(7, 5.5, 'No', 1, 0, 'C');
-            $pdf->Cell(90, 5.5, 'Keterangan', 1, 0, 'C');
-            $pdf->Cell(35, 5.5, 'Pemasukan', 1, 0, 'C');
-            $pdf->Cell(35, 5.5, 'Pengeluaran', 1, 0, 'C');
-            $pdf->Cell(35, 5.5, 'Jumlah', 1, 1, 'C');
+            $pdf->Cell(106, 5.5, 'Keterangan', 1, 0, 'C');
+            $pdf->Cell(30, 5.5, 'Pemasukan', 1, 0, 'C');
+            $pdf->Cell(30, 5.5, 'Pengeluaran', 1, 0, 'C');
+            $pdf->Cell(30, 5.5, 'Jumlah', 1, 1, 'C');
 
+            //Cashflow usaha 1
             $rp = $this->db->query("SELECT * FROM cashflow_b WHERE kode_perkiraan = '4.1.1'");
-            //tidak sama dengan 0, berarti ada isinya
-            if ($rp->result() > 0) {
+            if (!empty($rp->result())) {
                 $pdf->SetFont('Times', '', 12);
-                $pdf->Cell(7, 5.5, 'I', 1, 0, 'C');
-                $pdf->Cell(90, 5.5, 'USAHA 1', 1, 0, '');
-                $pdf->Cell(35, 5.5, '', 1, 0, 'C');
-                $pdf->Cell(35, 5.5, '', 1, 0, 'C');
-                $pdf->Cell(35, 5.5, '', 1, 1, 'C');
+                $pdf->Cell(7, 5.5, '', 1, 0, 'C');
+                $pdf->Cell(106, 5.5, 'USAHA 1', 1, 0, '');
+                $pdf->Cell(30, 5.5, '', 1, 0, 'C');
+                $pdf->Cell(30, 5.5, '', 1, 0, 'C');
+                $pdf->Cell(30, 5.5, '', 1, 1, 'C');
 
                 foreach ($rp->result() as $data) {
                     $pdf->Cell(7, 5.5, '', 1, 0, 'C');
-                    $pdf->Cell(90, 5.5, $data->keterangan, 1, 0, '');
-                    $pdf->Cell(35, 5.5, number_format($data->saldo), 1, 0, 'R');
-                    $pdf->Cell(35, 5.5, '', 1, 0, 'R');
-                    $pdf->Cell(35, 5.5, '', 1, 1, 'R');
+                    $pdf->Cell(106, 5.5, $data->keterangan, 1, 0, '');
+                    $pdf->Cell(30, 5.5, number_format($data->saldo), 1, 0, 'R');
+                    $pdf->Cell(30, 5.5, '', 1, 0, 'R');
+                    $pdf->Cell(30, 5.5, '', 1, 1, 'R');
                 }
                 $no = 0;
                 $rp = $this->db->query("SELECT * FROM cashflow_b WHERE MID(kode_perkiraan,1,3) = '5.1'");
                 foreach ($rp->result() as $data) {
                     $no++;
                     $pdf->Cell(7, 5.5, $no, 1, 0, 'C');
-                    $pdf->Cell(90, 5.5, $data->keterangan, 1, 0, '');
-                    $pdf->Cell(35, 5.5, '', 1, 0, 'R');
-                    $pdf->Cell(35, 5.5, number_format($data->saldo), 1, 0, 'R');
-                    $pdf->Cell(35, 5.5, '', 1, 1, 'R');
+                    $pdf->Cell(106, 5.5, $data->keterangan, 1, 0, '');
+                    $pdf->Cell(30, 5.5, '', 1, 0, 'R');
+                    $pdf->Cell(30, 5.5, number_format($data->saldo), 1, 0, 'R');
+                    $pdf->Cell(30, 5.5, '', 1, 1, 'R');
                 }
                 $pdf->Cell(7, 5.5, '', 1, 0, 'C');
-                $pdf->Cell(90, 5.5, 'SURPLUS USAHA 1', 1, 0, '');
-                $pdf->Cell(35, 5.5, '', 1, 0, 'C');
-                $pdf->Cell(35, 5.5, '', 1, 0, 'C');
-                $pdf->Cell(35, 5.5, '', 1, 1, 'C');
+                $pdf->Cell(106, 5.5, 'SURPLUS USAHA 1', 1, 0, '');
+                $pdf->Cell(30, 5.5, '', 1, 0, 'C');
+                $pdf->Cell(30, 5.5, '', 1, 0, 'C');
+                $pdf->Cell(30, 5.5, '', 1, 1, 'C');
             } else {
-                $pdf->SetFont('Times', '', 12);
             }
 
+            //Cashflow usaha 2
+            $rp2 = $this->db->query("SELECT * FROM cashflow_b WHERE kode_perkiraan = '4.1.2'");
+            if (!empty($rp2->result())) {
+                $pdf->SetFont('Times', '', 12);
+                $pdf->Cell(7, 5.5, '', 1, 0, 'C');
+                $pdf->Cell(106, 5.5, 'USAHA 2', 1, 0, '');
+                $pdf->Cell(30, 5.5, '', 1, 0, 'C');
+                $pdf->Cell(30, 5.5, '', 1, 0, 'C');
+                $pdf->Cell(30, 5.5, '', 1, 1, 'C');
 
+                foreach ($rp2->result() as $data) {
+                    $pdf->Cell(7, 5.5, '', 1, 0, 'C');
+                    $pdf->Cell(106, 5.5, $data->keterangan, 1, 0, '');
+                    $pdf->Cell(30, 5.5, number_format($data->saldo), 1, 0, 'R');
+                    $pdf->Cell(30, 5.5, '', 1, 0, 'R');
+                    $pdf->Cell(30, 5.5, '', 1, 1, 'R');
+                }
+                $no = 0;
+                $rp2 = $this->db->query("SELECT * FROM cashflow_b WHERE MID(kode_perkiraan,1,3) = '5.2'");
+                foreach ($rp2->result() as $data) {
+                    $no++;
+                    $pdf->Cell(7, 5.5, $no, 1, 0, 'C');
+                    $pdf->Cell(106, 5.5, $data->keterangan, 1, 0, '');
+                    $pdf->Cell(30, 5.5, '', 1, 0, 'R');
+                    $pdf->Cell(30, 5.5, number_format($data->saldo), 1, 0, 'R');
+                    $pdf->Cell(30, 5.5, '', 1, 1, 'R');
+                }
+                $pdf->Cell(7, 5.5, '', 1, 0, 'C');
+                $pdf->Cell(106, 5.5, 'SURPLUS USAHA 2', 1, 0, '');
+                $pdf->Cell(30, 5.5, '', 1, 0, 'C');
+                $pdf->Cell(30, 5.5, '', 1, 0, 'C');
+                $pdf->Cell(30, 5.5, '', 1, 1, 'C');
+            } else {
+            }
+
+            //Cashflow usaha 3
+            $rp3 = $this->db->query("SELECT * FROM cashflow_b WHERE kode_perkiraan = '4.1.3'");
+            if (!empty($rp3->result())) {
+                $pdf->Cell(7, 5.5, '', 1, 0, 'C');
+                $pdf->Cell(106, 5.5, 'USAHA 3', 1, 0, '');
+                $pdf->Cell(30, 5.5, '', 1, 0, 'C');
+                $pdf->Cell(30, 5.5, '', 1, 0, 'C');
+                $pdf->Cell(30, 5.5, '', 1, 1, 'C');
+
+                foreach ($rp3->result() as $data) {
+                    $pdf->Cell(7, 5.5, '', 1, 0, 'C');
+                    $pdf->Cell(106, 5.5, $data->keterangan, 1, 0, '');
+                    $pdf->Cell(30, 5.5, number_format($data->saldo), 1, 0, 'R');
+                    $pdf->Cell(30, 5.5, '', 1, 0, 'R');
+                    $pdf->Cell(30, 5.5, '', 1, 1, 'R');
+                }
+                $no = 0;
+                $rp3 = $this->db->query("SELECT * FROM cashflow_b WHERE MID(kode_perkiraan,1,3) = '5.3'");
+                foreach ($rp3->result() as $data) {
+                    $no++;
+                    $pdf->Cell(7, 5.5, $no, 1, 0, 'C');
+                    $pdf->Cell(106, 5.5, $data->keterangan, 1, 0, '');
+                    $pdf->Cell(30, 5.5, '', 1, 0, 'R');
+                    $pdf->Cell(30, 5.5, number_format($data->saldo), 1, 0, 'R');
+                    $pdf->Cell(30, 5.5, '', 1, 1, 'R');
+                }
+                $pdf->Cell(7, 5.5, '', 1, 0, 'C');
+                $pdf->Cell(106, 5.5, 'SURPLUS USAHA 3', 1, 0, '');
+                $pdf->Cell(30, 5.5, '', 1, 0, 'C');
+                $pdf->Cell(30, 5.5, '', 1, 0, 'C');
+                $pdf->Cell(30, 5.5, '', 1, 1, 'C');
+            } else {
+            }
+
+            //Surplus usaha
+            $pdf->Cell(7, 5.5, '', 1, 0, 'C');
+            $pdf->Cell(106, 5.5, 'JUMLAH PENDAPATAN DAN PENGELUARAN USAHA', 1, 0, '');
+            $pdf->Cell(30, 5.5, number_format($sum_pemasukan), 1, 0, 'R');
+            $pdf->Cell(30, 5.5, number_format($sum_pengeluaran), 1, 0, 'R');
+            $pdf->Cell(30, 5.5, number_format($surplus_usaha), 1, 1, 'R');
+
+            //Cashflow pengeluaran biaya lain
+            $rp4 = $this->db->query("SELECT * FROM cashflow_b WHERE MID(kode_perkiraan,1,3) = '5.4'");
+            if (!empty($rp4->result())) {
+                $pdf->SetFont('Times', '', 12);
+                $pdf->Cell(7, 5.5, '', 1, 0, 'C');
+                $pdf->Cell(106, 5.5, 'BIAYA LAIN-LAIN', 1, 0, '');
+                $pdf->Cell(30, 5.5, '', 1, 0, 'C');
+                $pdf->Cell(30, 5.5, '', 1, 0, 'C');
+                $pdf->Cell(30, 5.5, '', 1, 1, 'C');
+
+                $no = 0;
+                foreach ($rp4->result() as $data) {
+                    $no++;
+                    $pdf->Cell(7, 5.5, $no, 1, 0, 'C');
+                    $pdf->Cell(106, 5.5, $data->keterangan, 1, 0, '');
+                    $pdf->Cell(30, 5.5, '', 1, 0, 'R');
+                    $pdf->Cell(30, 5.5, number_format($data->saldo), 1, 0, 'R');
+                    $pdf->Cell(30, 5.5, '', 1, 1, 'R');
+                }
+                $pdf->Cell(7, 5.5, '', 1, 0, 'C');
+                $pdf->Cell(106, 5.5, 'JUMLAH BIAYA LAIN-LAIN', 1, 0, '');
+                $pdf->Cell(30, 5.5, '', 1, 0, 'C');
+                $pdf->Cell(30, 5.5, '', 1, 0, 'C');
+                $pdf->Cell(30, 5.5, number_format($sum_pengeluaran_lain), 1, 1, 'R');
+            } else {
+            }
+
+            //Cashflow pengeluaran angsuran
+            $angsuran = $this->db->query("SELECT * FROM cashflow_b WHERE MID(kode_perkiraan,1,3) = '5.5'");
+            if (!empty($angsuran->result())) {
+                $pdf->SetFont('Times', '', 12);
+                $pdf->Cell(7, 5.5, '', 1, 0, 'C');
+                $pdf->Cell(106, 5.5, 'BIAYA ANGSURAN HUTANG', 1, 0, '');
+                $pdf->Cell(30, 5.5, '', 1, 0, 'C');
+                $pdf->Cell(30, 5.5, '', 1, 0, 'C');
+                $pdf->Cell(30, 5.5, '', 1, 1, 'C');
+
+                $no = 0;
+                foreach ($angsuran->result() as $data) {
+                    $no++;
+                    $pdf->Cell(7, 5.5, $no, 1, 0, 'C');
+                    $pdf->Cell(106, 5.5, $data->keterangan, 1, 0, '');
+                    $pdf->Cell(30, 5.5, '', 1, 0, 'R');
+                    $pdf->Cell(30, 5.5, number_format($data->saldo), 1, 0, 'R');
+                    $pdf->Cell(30, 5.5, '', 1, 1, 'R');
+                }
+                $pdf->Cell(7, 5.5, '', 1, 0, 'C');
+                $pdf->Cell(106, 5.5, 'JUMLAH ANGSURAN HUTANG', 1, 0, '');
+                $pdf->Cell(30, 5.5, '', 1, 0, 'C');
+                $pdf->Cell(30, 5.5, '', 1, 0, 'C');
+                $pdf->Cell(30, 5.5, number_format($sum_pengeluaran_angsuran), 1, 1, 'R');
+            } else {
+            }
+
+            //Cashflow Rugi laba
+            $pdf->Cell(7, 5.5, '', 1, 0, 'C');
+            $pdf->Cell(106, 5.5, 'RUGI / LABA', 1, 0, '');
+            $pdf->Cell(30, 5.5, '', 1, 0, 'C');
+            $pdf->Cell(30, 5.5, '', 1, 0, 'C');
+            $pdf->Cell(30, 5.5, number_format($rugi_laba), 1, 1, 'R');
 
             $pdf->Output();
         }
